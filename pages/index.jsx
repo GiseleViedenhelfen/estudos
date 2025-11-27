42904c910a7253dd218582f367304194bd2c6e29
Gisele Viedenhelfen <gisele.viedenhelfen@gmail.com>
2025-11-27 15:27:10 -0300

    initial commit

---
+++ pages/index.jsx
@@ -0,0 +1,8 @@
+function Home() {
+  return (
+  <div style={{"display": "flex" ,"justifyContent": "center", "alignItems": "center", "height": "100vh"}}>
+    <span>vc faz tudo valer a pena s2</span>
+  </div>
+  );
+}
+export default Home;
